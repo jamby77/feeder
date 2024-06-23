22120c07f23c0b35f725861e0323181f8e489bdc
Petar Dzhambazov <jamby77@gmail.com>
2024-06-24 00:32:28 +0300

    implement form as non controlled

--- src/app/settings/config.tsx
+++ src/app/settings/config.tsx
@@ -1,105 +1,17 @@
 "use client";
 
-import { useEffect, useReducer } from "react";
+import { useRef } from "react";
 import { useAppContext } from "@/context/AppContext";
 import { commands } from "@/lib/commands";
-import { AppConfig, Shortcut } from "@/types";
-
-enum ConfigChange {
-  UPDATE_SHORTCUT = "UPDATE_SHORTCUT",
-  ADD_SHORTCUT = "ADD_SHORTCUT",
-  DELETE_SHORTCUT = "DELETE_SHORTCUT",
-  UPDATE = "UPDATE",
-  SET = "SET",
-}
-
-type SetPayload = {
-  config: AppConfig;
-};
-type UpdateFieldPayload = {
-  field: string;
-  value: any;
-};
-
-type DeleteShortcutPayload = {
-  key: string;
-};
-
-type UpdateShortcutPayload = {
-  key: string;
-  field: string;
-  value: any;
-};
-
-type ReducerAction =
-  | {
-      type: ConfigChange.UPDATE;
-      payload: UpdateFieldPayload;
-    }
-  | {
-      type: ConfigChange.UPDATE_SHORTCUT;
-      payload: UpdateShortcutPayload;
-    }
-  | {
-      type: ConfigChange.DELETE_SHORTCUT;
-      payload: DeleteShortcutPayload;
-    }
-  | {
-      type: ConfigChange.ADD_SHORTCUT;
-    }
-  | {
-      type: ConfigChange.SET;
-      payload: SetPayload;
-    };
-
-const reducer = (state: AppConfig, action: ReducerAction) => {
-  switch (action.type) {
-    case ConfigChange.UPDATE:
-      const field = action.payload.field;
-      return { ...state, [field]: action.payload.value };
-    case ConfigChange.ADD_SHORTCUT:
-      const sc: Shortcut = {
-        altKey: false,
-        command: "next",
-        ctrlKey: false,
-        metaKey: false,
-        shiftKey: false,
-        title: "",
-        key: "",
-      };
-      return { ...state, shortcuts: [...state.shortcuts, sc] };
-    case ConfigChange.DELETE_SHORTCUT: {
-      const shortcuts = state.shortcuts.filter(sc => sc.key !== action.payload.key);
-      return { ...state, shortcuts };
-    }
-    case ConfigChange.UPDATE_SHORTCUT: {
-      const { key, field, value } = action.payload;
-      const shortcuts = state.shortcuts.map((sc, idx) => {
-        if (`${sc.key}-${idx}` !== key) {
-          return sc;
-        }
-        return { ...sc, [field]: value };
-      });
-      return { ...state, shortcuts };
-    }
-
-    case ConfigChange.SET:
-      return { ...action.payload.config };
-  }
-  return state;
-};
+import { Shortcut } from "@/types";
 
 export const Config = ({}) => {
   const { config } = useAppContext();
 
-  const [state, dispatch] = useReducer(reducer, config || ({} as AppConfig));
-  useEffect(() => {
-    console.log("config changed");
-    if (!config) {
-      return;
-    }
-    dispatch({ type: ConfigChange.SET, payload: { config } });
-  }, [config]);
+  const idxRef = useRef(0);
+  if (!config) {
+    return null;
+  }
   const {
     title = "",
     refreshInterval = 10,
@@ -109,32 +21,45 @@ export const Config = ({}) => {
     hideRead = true,
     newOnTop = true,
     shortcuts = [],
-  } = state;
+  } = config;
+
   return (
     <div className="max-w-2xl text-gray-900 dark:text-gray-300">
-      <form name="config-form">
+      <form
+        name="config-form"
+        onSubmit={e => {
+          e.preventDefault();
+          e.stopPropagation();
+          const form = e.target as HTMLFormElement;
+          const formData = new FormData(form);
+          const shortcuts: Shortcut[] = [];
+          console.log({ shortcuts });
+          const configUpdate = {
+            ...config,
+          };
+          formData.forEach((value, field) => {
+            if (field.startsWith("shortcuts")) {
+              // process shortcuts separately
+              const [_, idx, shortCutFieldName] = field.split(".");
+              if (!shortcuts.at(+idx)) {
+                shortcuts[+idx] = {} as Shortcut;
+              }
+              // @ts-ignores
+              shortcuts[+idx][shortCutFieldName] = value;
+            } else {
+              // @ts-ignore
+              configUpdate[field] = value;
+            }
+          });
+          configUpdate.shortcuts = shortcuts;
+          console.log({ configUpdate });
+        }}
+      >
         <fieldset name="main" className="space-y-4 rounded-xl border-2 border-gray-700 p-4">
           <legend>Main</legend>
           <div className="input group flex flex-col">
             <label htmlFor="title">Title:</label>
-            <input
-              className="dark:bg-gray-700"
-              type="text"
-              id="title"
-              name="title"
-              value={title}
-              onChange={e => {
-                const value = e.target.value;
-                // console.log("change title", value);
-                dispatch({
-                  type: ConfigChange.UPDATE,
-                  payload: {
-                    field: "title",
-                    value,
-                  },
-                });
-              }}
-            />
+            <input className="dark:bg-gray-700" type="text" id="title" name="title" defaultValue={title} />
           </div>
           <div className="input group flex flex-col">
             <label htmlFor="refreshInterval">Refresh Interval (in sec):</label>
@@ -143,65 +68,16 @@ export const Config = ({}) => {
               type="number"
               id="refreshInterval"
               name="refreshInterval"
-              value={refreshInterval}
-              onChange={e => {
-                const value = +e.target.value;
-                if (isNaN(value)) {
-                  console.log("Invalid number");
-                  return;
-                }
-                // console.log("change title", value);
-                dispatch({
-                  type: ConfigChange.UPDATE,
-                  payload: {
-                    field: "refreshInterval",
-                    value,
-                  },
-                });
-              }}
+              defaultValue={refreshInterval}
             />
           </div>
           <div className="input group flex flex-row gap-2">
             <label htmlFor="newOnTop">New Items on Top:</label>
-            <input
-              type="checkbox"
-              id="newOnTop"
-              name="newOnTop"
-              checked={newOnTop}
-              onChange={e => {
-                const value = e.target.checked;
-                const field = e.target.name;
-                // console.log("change title", value);
-                dispatch({
-                  type: ConfigChange.UPDATE,
-                  payload: {
-                    field,
-                    value,
-                  },
-                });
-              }}
-            />
+            <input type="checkbox" id="newOnTop" name="newOnTop" defaultChecked={newOnTop} />
           </div>
           <div className="input group flex flex-row gap-2">
             <label htmlFor="hideRead">Hide Read Items:</label>
-            <input
-              type="checkbox"
-              id="hideRead"
-              name="hideRead"
-              checked={hideRead}
-              onChange={e => {
-                const value = e.target.checked;
-                const field = e.target.name;
-                // console.log("change title", value);
-                dispatch({
-                  type: ConfigChange.UPDATE,
-                  payload: {
-                    field,
-                    value,
-                  },
-                });
-              }}
-            />
+            <input type="checkbox" id="hideRead" name="hideRead" defaultChecked={hideRead} />
           </div>
           <div className="input group flex flex-row gap-2">
             <label htmlFor="hideEmptyCategories">Hide Empty Categories:</label>
@@ -209,62 +85,16 @@ export const Config = ({}) => {
               type="checkbox"
               id="hideEmptyCategories"
               name="hideEmptyCategories"
-              checked={hideEmptyCategories}
-              onChange={e => {
-                const value = e.target.checked;
-                const field = e.target.name;
-                // console.log("change title", value);
-                dispatch({
-                  type: ConfigChange.UPDATE,
-                  payload: {
-                    field,
-                    value,
-                  },
-                });
-              }}
+              defaultChecked={hideEmptyCategories}
             />
           </div>
           <div className="input group flex flex-row gap-2">
             <label htmlFor="hideEmptyFeeds">Hide Empty Feeds:</label>
-            <input
-              type="checkbox"
-              id="hideEmptyFeeds"
-              name="hideEmptyFeeds"
-              checked={hideEmptyFeeds}
-              onChange={e => {
-                const value = e.target.checked;
-                const field = e.target.name;
-                // console.log("change title", value);
-                dispatch({
-                  type: ConfigChange.UPDATE,
-                  payload: {
-                    field,
-                    value,
-                  },
-                });
-              }}
-            />
+            <input type="checkbox" id="hideEmptyFeeds" name="hideEmptyFeeds" defaultChecked={hideEmptyFeeds} />
           </div>
           <div className="input group flex flex-row gap-2">
             <label htmlFor="enableShortcuts">Enable Shortcuts:</label>
-            <input
-              type="checkbox"
-              id="enableShortcuts"
-              name="enableShortcuts"
-              checked={enableShortcuts}
-              onChange={e => {
-                const value = e.target.checked;
-                const field = e.target.name;
-                // console.log("change title", value);
-                dispatch({
-                  type: ConfigChange.UPDATE,
-                  payload: {
-                    field,
-                    value,
-                  },
-                });
-              }}
-            />
+            <input type="checkbox" id="enableShortcuts" name="enableShortcuts" defaultChecked={enableShortcuts} />
           </div>
         </fieldset>
         <fieldset
@@ -276,6 +106,9 @@ export const Config = ({}) => {
           {shortcuts.map((sc, idx) => {
             const { key, title, altKey, ctrlKey, command, metaKey, shiftKey } = sc;
             const keyIdx = `${key}-${idx}`;
+            const namePrefix = `shortcuts.${idx}`;
+            const mkName = (name = "") => `${namePrefix}.${name}`;
+            idxRef.current = idx;
             return (
               <div
                 key={keyIdx}
@@ -287,18 +120,8 @@ export const Config = ({}) => {
                     className="dark:bg-gray-700"
                     type="text"
                     id={`title-${keyIdx}`}
-                    name="title"
-                    value={title}
-                    onChange={e => {
-                      let value = e.target.value;
-
-                      const field = e.target.name;
-                      // console.log("change title", value);
-                      dispatch({
-                        type: ConfigChange.UPDATE_SHORTCUT,
-                        payload: { field, value, key: keyIdx },
-                      });
-                    }}
+                    name={mkName("title")}
+                    defaultValue={title}
                   />
                 </div>
                 <div className="input group flex flex-col">
@@ -306,21 +129,8 @@ export const Config = ({}) => {
                   <select
                     className="dark:bg-gray-700"
                     id={`command-${keyIdx}`}
-                    name="command"
-                    value={command}
-                    onChange={e => {
-                      let value = e.target.value;
-
-                      const field = e.target.name;
-                      console.log({
-                        field,
-                        value,
-                      });
-                      dispatch({
-                        type: ConfigChange.UPDATE_SHORTCUT,
-                        payload: { field, value, key: keyIdx },
-                      });
-                    }}
+                    name={mkName("command")}
+                    defaultValue={command}
                   >
                     {Object.entries(commands).map(([cmd, txt]) => {
                       return (
@@ -341,114 +151,30 @@ export const Config = ({}) => {
                       maxLength={1}
                       minLength={1}
                       id={`key-${keyIdx}`}
-                      name="key"
-                      value={key}
-                      onChange={e => {
-                        let value = e.target.value;
-                        if (value.length > 1) {
-                          // shortcut key max 1 char length
-                          value = value[0];
-                        }
-                        const field = e.target.name;
-                        // console.log("change title", value);
-                        dispatch({
-                          type: ConfigChange.UPDATE_SHORTCUT,
-                          payload: {
-                            field,
-                            value,
-                            key: keyIdx,
-                          },
-                        });
-                      }}
+                      name={mkName("key")}
+                      defaultValue={key}
                     />
                   </div>
                   <div className="flex flex-row items-center gap-2">
                     <label htmlFor={`altKey-${keyIdx}`}>Alt + </label>
-                    <input
-                      type="checkbox"
-                      id={`altKey-${keyIdx}`}
-                      name="altKey"
-                      checked={altKey}
-                      onChange={e => {
-                        const value = e.target.checked;
-                        const field = e.target.name;
-                        // console.log("change title", value);
-                        dispatch({
-                          type: ConfigChange.UPDATE_SHORTCUT,
-                          payload: {
-                            field,
-                            value,
-                            key: keyIdx,
-                          },
-                        });
-                      }}
-                    />
+                    <input type="checkbox" id={`altKey-${keyIdx}`} name={mkName("altKey")} defaultChecked={altKey} />
                   </div>
                   <div className="flex flex-row items-center gap-2">
                     <label htmlFor={`ctrlKey-${keyIdx}`}>Ctrl + </label>
-                    <input
-                      type="checkbox"
-                      id={`ctrlKey-${keyIdx}`}
-                      name="ctrlKey"
-                      checked={ctrlKey}
-                      onChange={e => {
-                        const value = e.target.checked;
-                        const field = e.target.name;
-                        // console.log("change title", value);
-                        dispatch({
-                          type: ConfigChange.UPDATE_SHORTCUT,
-                          payload: {
-                            field,
-                            value,
-                            key: keyIdx,
-                          },
-                        });
-                      }}
-                    />
+                    <input type="checkbox" id={`ctrlKey-${keyIdx}`} name={mkName("ctrlKey")} defaultChecked={ctrlKey} />
                   </div>
                   <div className="flex flex-row items-center gap-2">
                     <label htmlFor={`shiftKey-${keyIdx}`}>Shift + </label>
                     <input
                       type="checkbox"
                       id={`shiftKey-${keyIdx}`}
-                      name="shiftKey"
-                      checked={shiftKey}
-                      onChange={e => {
-                        const value = e.target.checked;
-                        const field = e.target.name;
-                        // console.log("change title", value);
-                        dispatch({
-                          type: ConfigChange.UPDATE_SHORTCUT,
-                          payload: {
-                            field,
-                            value,
-                            key: keyIdx,
-                          },
-                        });
-                      }}
+                      name={mkName("shiftKey")}
+                      defaultChecked={shiftKey}
                     />
                   </div>
                   <div className="flex flex-row items-center gap-2">
                     <label htmlFor={`metaKey-${keyIdx}`}>Meta + </label>
-                    <input
-                      type="checkbox"
-                      id={`metaKey-${keyIdx}`}
-                      name="metaKey"
-                      checked={metaKey}
-                      onChange={e => {
-                        const value = e.target.checked;
-                        const field = e.target.name;
-                        // console.log("change title", value);
-                        dispatch({
-                          type: ConfigChange.UPDATE_SHORTCUT,
-                          payload: {
-                            field,
-                            value,
-                            key: keyIdx,
-                          },
-                        });
-                      }}
-                    />
+                    <input type="checkbox" id={`metaKey-${keyIdx}`} name={mkName("metaKey")} defaultChecked={metaKey} />
                   </div>
                 </div>
                 <button>🗑️</button>
@@ -456,6 +182,9 @@ export const Config = ({}) => {
             );
           })}
         </fieldset>
+        <button type="submit" className="mt-4 w-full rounded-full border px-2 py-1 md:max-w-36 md:px-4 md:py-4">
+          Save Config
+        </button>
       </form>
     </div>
   );
