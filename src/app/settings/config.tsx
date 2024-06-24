99013e5b8ebd91d6b968e0645113cead27798d00
Petar Dzhambazov <jamby77@gmail.com>
2024-06-24 11:06:11 +0300

    refactor shortcuts form to separate component

--- src/app/settings/config.tsx
+++ src/app/settings/config.tsx
@@ -1,14 +1,25 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect, useMemo, useState } from "react";
+import Shortcut from "@/app/settings/shortcut";
 import { useAppContext } from "@/context/AppContext";
-import { commands } from "@/lib/commands";
-import { Shortcut } from "@/types";
+import { Shortcut as ShortcutType } from "@/types";
 
 export const Config = ({}) => {
   const { config } = useAppContext();
 
-  const idxRef = useRef(0);
+  const [idxState, setIdxState] = useState(0);
+  const scArray = useMemo(() => {
+    return new Array(idxState).fill(0);
+  }, [idxState]);
+
+  useEffect(() => {
+    if (!config || !config.shortcuts) {
+      return;
+    }
+    setIdxState(config.shortcuts.length);
+  }, [config]);
+
   if (!config) {
     return null;
   }
@@ -22,7 +33,6 @@ export const Config = ({}) => {
     newOnTop = true,
     shortcuts = [],
   } = config;
-
   return (
     <div className="max-w-2xl text-gray-900 dark:text-gray-300">
       <form
@@ -32,7 +42,7 @@ export const Config = ({}) => {
           e.stopPropagation();
           const form = e.target as HTMLFormElement;
           const formData = new FormData(form);
-          const shortcuts: Shortcut[] = [];
+          const shortcuts: ShortcutType[] = [];
           console.log({ shortcuts });
           const configUpdate = {
             ...config,
@@ -42,7 +52,7 @@ export const Config = ({}) => {
               // process shortcuts separately
               const [_, idx, shortCutFieldName] = field.split(".");
               if (!shortcuts.at(+idx)) {
-                shortcuts[+idx] = {} as Shortcut;
+                shortcuts[+idx] = {} as ShortcutType;
               }
               // @ts-ignores
               shortcuts[+idx][shortCutFieldName] = value;
@@ -103,84 +113,23 @@ export const Config = ({}) => {
           className="space-y-4 rounded-xl border-2 border-gray-700 p-4"
         >
           <legend>Shortcuts</legend>
-          {shortcuts.map((sc, idx) => {
-            const { key, title, altKey, ctrlKey, command, metaKey, shiftKey } = sc;
-            const keyIdx = `${key}-${idx}`;
-            const namePrefix = `shortcuts.${idx}`;
-            const mkName = (name = "") => `${namePrefix}.${name}`;
-            idxRef.current = idx;
-            return (
-              <div
-                key={keyIdx}
-                className="space-y-4 border-b-2 border-gray-700 py-4 first:pt-0 last:border-0 last:pb-0"
-              >
-                <div className="input group flex flex-col">
-                  <label htmlFor={`title-${keyIdx}`}>Shortcut Title:</label>
-                  <input
-                    className="dark:bg-gray-700"
-                    type="text"
-                    id={`title-${keyIdx}`}
-                    name={mkName("title")}
-                    defaultValue={title}
-                  />
-                </div>
-                <div className="input group flex flex-col">
-                  <label htmlFor={`command-${keyIdx}`}>Shortcut Command:</label>
-                  <select
-                    className="dark:bg-gray-700"
-                    id={`command-${keyIdx}`}
-                    name={mkName("command")}
-                    defaultValue={command}
-                  >
-                    {Object.entries(commands).map(([cmd, txt]) => {
-                      return (
-                        <option key={cmd} value={cmd}>
-                          {txt}
-                        </option>
-                      );
-                    })}
-                  </select>
-                </div>
 
-                <div className="flex flex-row gap-2">
-                  <div className="flex flex-row items-center gap-2">
-                    <label htmlFor={`key-${keyIdx}`}>Shortcut:</label>
-                    <input
-                      className="w-12 max-w-12 px-4 dark:bg-gray-700"
-                      type="text"
-                      maxLength={1}
-                      minLength={1}
-                      id={`key-${keyIdx}`}
-                      name={mkName("key")}
-                      defaultValue={key}
-                    />
-                  </div>
-                  <div className="flex flex-row items-center gap-2">
-                    <label htmlFor={`altKey-${keyIdx}`}>Alt + </label>
-                    <input type="checkbox" id={`altKey-${keyIdx}`} name={mkName("altKey")} defaultChecked={altKey} />
-                  </div>
-                  <div className="flex flex-row items-center gap-2">
-                    <label htmlFor={`ctrlKey-${keyIdx}`}>Ctrl + </label>
-                    <input type="checkbox" id={`ctrlKey-${keyIdx}`} name={mkName("ctrlKey")} defaultChecked={ctrlKey} />
-                  </div>
-                  <div className="flex flex-row items-center gap-2">
-                    <label htmlFor={`shiftKey-${keyIdx}`}>Shift + </label>
-                    <input
-                      type="checkbox"
-                      id={`shiftKey-${keyIdx}`}
-                      name={mkName("shiftKey")}
-                      defaultChecked={shiftKey}
-                    />
-                  </div>
-                  <div className="flex flex-row items-center gap-2">
-                    <label htmlFor={`metaKey-${keyIdx}`}>Meta + </label>
-                    <input type="checkbox" id={`metaKey-${keyIdx}`} name={mkName("metaKey")} defaultChecked={metaKey} />
-                  </div>
-                </div>
-                <button>🗑️</button>
-              </div>
-            );
+          {scArray.map((_, idx) => {
+            const sc = shortcuts.at(idx);
+            // idxRef.current = idx;
+            return <Shortcut idx={idx} shortcut={sc} />;
           })}
+          <div>
+            <button
+              type="button"
+              className="ml-auto block h-16 w-16 rounded-full bg-gray-500 p-2 text-4xl leading-none text-blue-400 shadow-xl backdrop-blur"
+              onClick={() => {
+                setIdxState(idxState => idxState + 1);
+              }}
+            >
+              +
+            </button>
+          </div>
         </fieldset>
         <button type="submit" className="mt-4 w-full rounded-full border px-2 py-1 md:max-w-36 md:px-4 md:py-4">
           Save Config
---
+++ src/app/settings/shortcut.tsx
@@ -0,0 +1,70 @@
+import { commands } from "@/lib/commands";
+import { Shortcut as ShortcutType } from "@/types";
+
+export const Shortcut = ({ shortcut, idx }: { shortcut?: ShortcutType; idx: number }) => {
+  let sc = shortcut ?? ({} as ShortcutType);
+  const { key = "__new__", title, altKey, ctrlKey, command, metaKey, shiftKey } = sc;
+  const keyIdx = `${key}-${idx}`;
+  const namePrefix = `shortcuts.${idx}`;
+  const mkName = (name = "") => `${namePrefix}.${name}`;
+  return (
+    <div className="grid grid-cols-2 grid-rows-3 place-items-start gap-4 border-b-2 border-gray-700 py-4 first:pt-0 last:border-0 last:pb-0">
+      <div className="input group flex flex-col">
+        <label htmlFor={`title-${keyIdx}`}>Shortcut Title:</label>
+        <input
+          className="dark:bg-gray-700"
+          type="text"
+          id={`title-${keyIdx}`}
+          name={mkName("title")}
+          defaultValue={title}
+        />
+      </div>
+      <div className="input group flex flex-col">
+        <label htmlFor={`command-${keyIdx}`}>Shortcut Command:</label>
+        <select className="dark:bg-gray-700" id={`command-${keyIdx}`} name={mkName("command")} defaultValue={command}>
+          {Object.entries(commands).map(([cmd, txt]) => {
+            return (
+              <option key={cmd} value={cmd}>
+                {txt}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+
+      <div className="col-span-2 flex flex-row gap-2">
+        <div className="flex flex-row items-center gap-2">
+          <label htmlFor={`key-${keyIdx}`}>Shortcut:</label>
+          <input
+            className="w-12 max-w-12 px-4 dark:bg-gray-700"
+            type="text"
+            maxLength={1}
+            minLength={1}
+            id={`key-${keyIdx}`}
+            name={mkName("key")}
+            defaultValue={key}
+          />
+        </div>
+        <div className="flex flex-row items-center gap-2">
+          <label htmlFor={`altKey-${keyIdx}`}>Alt + </label>
+          <input type="checkbox" id={`altKey-${keyIdx}`} name={mkName("altKey")} defaultChecked={altKey} />
+        </div>
+        <div className="flex flex-row items-center gap-2">
+          <label htmlFor={`ctrlKey-${keyIdx}`}>Ctrl + </label>
+          <input type="checkbox" id={`ctrlKey-${keyIdx}`} name={mkName("ctrlKey")} defaultChecked={ctrlKey} />
+        </div>
+        <div className="flex flex-row items-center gap-2">
+          <label htmlFor={`shiftKey-${keyIdx}`}>Shift + </label>
+          <input type="checkbox" id={`shiftKey-${keyIdx}`} name={mkName("shiftKey")} defaultChecked={shiftKey} />
+        </div>
+        <div className="flex flex-row items-center gap-2">
+          <label htmlFor={`metaKey-${keyIdx}`}>Meta + </label>
+          <input type="checkbox" id={`metaKey-${keyIdx}`} name={mkName("metaKey")} defaultChecked={metaKey} />
+        </div>
+      </div>
+      <button>🗑️</button>
+    </div>
+  );
+};
+
+export default Shortcut;
